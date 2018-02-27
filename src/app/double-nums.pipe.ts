c84313ca0c8d126d4f15dcab04e70b90d6070370
Van Pho Nguyen <vanpho02@gmail.com>
2018-02-27 20:41:37 +0700

    keyof

--- src/app/double-nums.pipe.ts
+++ src/app/double-nums.pipe.ts
@@ -5,5 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DoubleNumsPipe implements PipeTransform {
     transform(value: number[]) {
         return value.map(v => v * 2);
+        // return value.filter(v => v % 2 === 0);
     }
 }
+/*
+interface Person {
+    name: string;
+    age: number;
+}
+
+const teo: Person = { name: 'a', age: 10 };
+
+function showPropsValue(p: Person, propName: keyof Person) {
+    console.log(p[propName]);
+}
+
+showPropsValue(teo, 'name')
+*/
