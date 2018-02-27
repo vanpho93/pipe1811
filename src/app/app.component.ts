15bc1a8f534873ba7085eafd6d158abb29d48952
Van Pho Nguyen <vanpho02@gmail.com>
2018-02-27 20:47:50 +0700

    app.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Person } from './types';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,9 @@ export class AppComponent {
   height = Promise.resolve(100);
   grade = 5.5;
   nums = [1, 3, 4, 2, 6];
+  students: Person[] = [
+    { name: 'Ti', age: 10, height: 100 },
+    { name: 'Teo', age: 14, height: 90 },
+    { name: 'Tun', age: 13, height: 150 },
+  ];
 }
